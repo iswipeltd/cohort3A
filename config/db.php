46afdec124bc28bfa3c43726472f44d0cecebfbd
Ifeanyi Favour <ifeanyifavour041@gmail.com>
2026-05-25 23:30:52 +0100

    "Fix Railway MySQL connection"

--- config/db.php
+++ config/db.php
@@ -4,7 +4,13 @@ $user = "root";
 $password = "";
 $database = "hr_suite_db";
 
-$conn = mysqli_connect(mysql.railway.internal, root, QQTNGduqoNDquPCINkcVtfyujjLNapDE, railway, 3306);
+$conn = mysqli_connect(
+    "zephyr.proxy.rlwy.net",
+    "root",
+    "YOUR_PASSWORD_HERE",
+    "railway",
+    46894
+);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
